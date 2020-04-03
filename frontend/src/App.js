1e27e338c45dadb643ebcb18271261f490757da5
Micaella Gouveia <micaella2212@gmail.com>
2020-04-02 22:19:36 -0300

    Add colors and responsive screen

---
+++ frontend/src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+
+
+import GlobalStyle from "./styles/global";
+import { Container, Content } from "./styles";
+
+import Upload from "./components/Upload";
+
+class App extends Component {
+
+  render() {
+    
+    return (
+      <Container>
+        <Content>
+         Teste
+        </Content>
+        <GlobalStyle />
+      </Container>
+    );
+  }
+}
+
+export default App;
